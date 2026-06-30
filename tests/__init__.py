69d2c430721b280ed402e0432228eb1474991310
Marlon Costa <marlon.costa@datacosmos.com.br>
2026-06-29 22:39:09 -0300

    Regenerate lazy wrapper exports

--- tests/__init__.py
+++ tests/__init__.py
@@ -3,12 +3,66 @@
 
 from __future__ import annotations
 
+from typing import TYPE_CHECKING
+
 from flext_core.lazy import (
     build_lazy_import_map,
     install_lazy_exports,
     merge_lazy_imports,
 )
 
+if TYPE_CHECKING:
+    from flext_tests import (
+        d as d,
+        e as e,
+        h as h,
+        r as r,
+        td as td,
+        tf as tf,
+        tk as tk,
+        tm as tm,
+        tv as tv,
+        x as x,
+    )
+
+    from tests.base import (
+        TestsFlextTapLdapServiceBase as TestsFlextTapLdapServiceBase,
+        s as s,
+    )
+    from tests.constants import (
+        TestsFlextTapLdapConstants as TestsFlextTapLdapConstants,
+        c as c,
+    )
+    from tests.e2e.test_integration import (
+        TestsFlextTapLdapIntegration as TestsFlextTapLdapIntegration,
+    )
+    from tests.models import TestsFlextTapLdapModels as TestsFlextTapLdapModels, m as m
+    from tests.protocols import (
+        TestsFlextTapLdapProtocols as TestsFlextTapLdapProtocols,
+        p as p,
+    )
+    from tests.settings import TestsFlextTapLdapSettings as TestsFlextTapLdapSettings
+    from tests.typings import TestsFlextTapLdapTypes as TestsFlextTapLdapTypes, t as t
+    from tests.unit.test_client import (
+        TestsFlextTapLdapClient as TestsFlextTapLdapClient,
+    )
+    from tests.unit.test_client_quick import (
+        TestsFlextTapLdapClientQuick as TestsFlextTapLdapClientQuick,
+    )
+    from tests.unit.test_ldif_processor import (
+        TestsFlextTapLdapLdifProcessor as TestsFlextTapLdapLdifProcessor,
+    )
+    from tests.unit.test_ldif_stream import (
+        TestsFlextTapLdapLdifStream as TestsFlextTapLdapLdifStream,
+    )
+    from tests.unit.test_models import (
+        TestsFlextTapLdapModelsUnit as TestsFlextTapLdapModelsUnit,
+    )
+    from tests.unit.test_tap import TestsFlextTapLdapTap as TestsFlextTapLdapTap
+    from tests.utilities import (
+        TestsFlextTapLdapUtilities as TestsFlextTapLdapUtilities,
+        u as u,
+    )
 _LAZY_IMPORTS = merge_lazy_imports(
     (
         ".e2e",
