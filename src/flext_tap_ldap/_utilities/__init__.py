016f537b8831caa177cfe5fac089c6cd8278d5a7
Marlon Costa <marlon.costa@datacosmos.com.br>
2026-07-06 00:21:37 -0300

    fixes

--- src/flext_tap_ldap/_utilities/__init__.py
+++ src/flext_tap_ldap/_utilities/__init__.py
@@ -8,14 +8,12 @@ from typing import TYPE_CHECKING
 from flext_core.lazy import build_lazy_import_map, install_lazy_exports
 
 if TYPE_CHECKING:
-    from flext_tap_ldap._utilities._processor import (
-        FlextTapLdapUtilitiesProcessorMixin as FlextTapLdapUtilitiesProcessorMixin,
-    )
+    from flext_tap_ldap._utilities._processor import FlextTapLdapUtilitiesProcessorMixin
     from flext_tap_ldap._utilities.client_support import (
-        FlextTapLdapUtilitiesClientSupport as FlextTapLdapUtilitiesClientSupport,
+        FlextTapLdapUtilitiesClientSupport,
     )
     from flext_tap_ldap._utilities.error_handling import (
-        FlextTapLdapUtilitiesErrorHandling as FlextTapLdapUtilitiesErrorHandling,
+        FlextTapLdapUtilitiesErrorHandling,
     )
 _LAZY_IMPORTS = build_lazy_import_map(
     {
