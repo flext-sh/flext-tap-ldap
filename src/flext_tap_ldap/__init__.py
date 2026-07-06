fa25ec2ecafdcfb64bc3c5c5a2bb460918e507aa
Marlon Costa <marlon.costa@datacosmos.com.br>
2026-07-06 02:46:40 -0300

    chore(codegen): inline lazy __init__.py, eliminate _exports.py sidecar
    Regenerated by 'flext_infra codegen init --apply': PEP 562 lazy imports are
    now inlined directly into each package __init__.py; the separate _exports.py
    lazy-map sidecar is removed (superseded by the inline map).

--- src/flext_tap_ldap/__init__.py
+++ src/flext_tap_ldap/__init__.py
@@ -5,7 +5,7 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-from flext_core.lazy import install_lazy_exports
+from flext_core.lazy import build_lazy_import_map, install_lazy_exports
 from flext_tap_ldap.__version__ import (
     __author__,
     __author_email__,
@@ -16,77 +16,56 @@ from flext_tap_ldap.__version__ import (
     __version__,
     __version_info__,
 )
-from flext_tap_ldap._exports import FLEXT_TAP_LDAP_LAZY_IMPORTS
 
 if TYPE_CHECKING:
-    from flext_ldap import d as d, e as e, h as h, r as r, s as s, x as x
-    from flext_tap_ldap.api import (
-        FlextTapLdapService as FlextTapLdapService,
-        tap_ldap as tap_ldap,
-    )
-    from flext_tap_ldap.constants import (
-        FlextTapLdapConstants as FlextTapLdapConstants,
-        c as c,
-    )
-    from flext_tap_ldap.models import FlextTapLdapModels as FlextTapLdapModels, m as m
-    from flext_tap_ldap.protocols import (
-        FlextTapLdapProtocols as FlextTapLdapProtocols,
-        p as p,
-    )
-    from flext_tap_ldap.settings import FlextTapLdapSettings as FlextTapLdapSettings
-    from flext_tap_ldap.tap import FlextTapLdapTap as FlextTapLdapTap
-    from flext_tap_ldap.typings import FlextTapLdapTypes as FlextTapLdapTypes, t as t
-    from flext_tap_ldap.utilities import (
-        FlextTapLdapUtilities as FlextTapLdapUtilities,
-        u as u,
-    )
-
-
-_LAZY_IMPORTS = FLEXT_TAP_LDAP_LAZY_IMPORTS
-
-
-_EAGER_EXPORTS = (
-    __author__,
-    __author_email__,
-    __description__,
-    __license__,
-    __title__,
-    __url__,
-    __version__,
-    __version_info__,
+    from flext_ldap import d, e, h, r, s, x
+    from flext_tap_ldap.api import FlextTapLdapService
+    from flext_tap_ldap.constants import FlextTapLdapConstants, c
+    from flext_tap_ldap.models import FlextTapLdapModels, m
+    from flext_tap_ldap.protocols import FlextTapLdapProtocols, p
+    from flext_tap_ldap.settings import FlextTapLdapSettings
+    from flext_tap_ldap.tap import FlextTapLdapTap
+    from flext_tap_ldap.typings import FlextTapLdapTypes, t
+    from flext_tap_ldap.utilities import FlextTapLdapUtilities, u
+_LAZY_IMPORTS = build_lazy_import_map(
+    {
+        ".api": (
+            "FlextTapLdapService",
+            "tap_ldap",
+        ),
+        ".constants": (
+            "FlextTapLdapConstants",
+            "c",
+        ),
+        ".models": (
+            "FlextTapLdapModels",
+            "m",
+        ),
+        ".protocols": (
+            "FlextTapLdapProtocols",
+            "p",
+        ),
+        ".settings": ("FlextTapLdapSettings",),
+        ".tap": ("FlextTapLdapTap",),
+        ".typings": (
+            "FlextTapLdapTypes",
+            "t",
+        ),
+        ".utilities": (
+            "FlextTapLdapUtilities",
+            "u",
+        ),
+        "flext_ldap": (
+            "d",
+            "e",
+            "h",
+            "r",
+            "s",
+            "x",
+        ),
+    },
 )
 
-
-_PUBLIC_EXPORTS: tuple[str, ...] = (
-    "FlextTapLdapConstants",
-    "FlextTapLdapModels",
-    "FlextTapLdapProtocols",
-    "FlextTapLdapService",
-    "FlextTapLdapSettings",
-    "FlextTapLdapTap",
-    "FlextTapLdapTypes",
-    "FlextTapLdapUtilities",
-    "tap_ldap",
-    "__author__",
-    "__author_email__",
-    "__description__",
-    "__license__",
-    "__title__",
-    "__url__",
-    "__version__",
-    "__version_info__",
-    "c",
-    "d",
-    "e",
-    "h",
-    "m",
-    "p",
-    "r",
-    "s",
-    "t",
-    "u",
-    "x",
-)
 
 __all__: tuple[str, ...] = (
     "FlextTapLdapConstants",
@@ -124,5 +103,5 @@ install_lazy_exports(
     __name__,
     globals(),
     _LAZY_IMPORTS,
-    public_exports=_PUBLIC_EXPORTS,
+    public_exports=__all__,
 )
