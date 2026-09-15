86e8bbd471fdf39a2dff327b52b324024ce64941
Marlon Costa <marlon.costa@datacosmos.com.br>
2026-09-15 08:46:26 -0300

    chore: adopt canonical gen convergence (dolt_mode restored, uv workspace tables removed)

--- src/flext_tap_ldap/__init__.py
+++ src/flext_tap_ldap/__init__.py
@@ -20,7 +20,8 @@ from .__version__ import (
 )
 
 if TYPE_CHECKING:
-    from flext_ldap import FlextLdapConstants, d, e, h, r, x
+    from flext_ldap import FlextLdapConstants
+    from flext_meltano import d, e, h, r, x
 
     from . import services
     from ._config import FlextTapLdapConfig, config
@@ -87,7 +88,8 @@ _LAZY_IMPORTS = MappingProxyType(
             ".services.extract": ("FlextTapLdapExtractService",),
             ".typings": ("FlextTapLdapTypes", "t"),
             ".utilities": ("FlextTapLdapUtilities", "u"),
-            "flext_ldap": ("FlextLdapConstants", "d", "e", "h", "r", "x"),
+            "flext_ldap": ("FlextLdapConstants",),
+            "flext_meltano": ("d", "e", "h", "r", "x"),
         }),
         alias_groups=MappingProxyType({}),
         sort_keys=False,
