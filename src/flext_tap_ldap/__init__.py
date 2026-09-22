dafa59c005161533e5f8ad3c9a18c103ace882bb
Marlon Costa <marlon.costa@datacosmos.com.br>
2026-09-22 11:31:54 -0300

    [WIP] checkpoint recovered changes for current integration adoption

--- src/flext_tap_ldap/__init__.py
+++ src/flext_tap_ldap/__init__.py
@@ -20,23 +20,26 @@ from .__version__ import (
 )
 
 if TYPE_CHECKING:
-    from flext_meltano import c as meltano_c
+    from flext_cli import cli
+    from flext_ldap import ldap
+    from flext_ldif import ldif
+    from flext_meltano import meltano
+    from pydantic_core import from_json, to_json, to_jsonable_python
 
-    from flext_core import d, e, h, r, x
+    from flext_core import core, d, e, h, lazy_attribute, r, x
 
     from . import services
-    from .__version__ import FlextTapLdapVersion
     from ._config import FlextTapLdapConfig, config
     from ._settings import FlextTapLdapSettings, settings
     from .api import FlextTapLdapService, tap_ldap
     from .base import FlextTapLdapServiceBase, FlextTapLdapServiceBase as s
     from .cli import main
-    from .constants import FlextTapLdapConstants, FlextTapLdapConstants as c
-    from .models import FlextTapLdapModels, FlextTapLdapModels as m
-    from .protocols import FlextTapLdapProtocols, FlextTapLdapProtocols as p
+    from .constants import FlextTapLdapConstants, c
+    from .models import FlextTapLdapModels, m
+    from .protocols import FlextTapLdapProtocols, p
     from .services.extract import FlextTapLdapExtractService
     from .typings import FlextTapLdapTypes, FlextTapLdapTypes as t
-    from .utilities import FlextTapLdapUtilities, FlextTapLdapUtilities as u
+    from .utilities import FlextTapLdapUtilities, u
 __all__: tuple[str, ...] = (
     "FlextTapLdapConfig",
     "FlextTapLdapConstants",
@@ -48,7 +51,6 @@ __all__: tuple[str, ...] = (
     "FlextTapLdapSettings",
     "FlextTapLdapTypes",
     "FlextTapLdapUtilities",
-    "FlextTapLdapVersion",
     "__author__",
     "__author_email__",
     "__description__",
@@ -58,13 +60,19 @@ __all__: tuple[str, ...] = (
     "__version__",
     "__version_info__",
     "c",
+    "cli",
     "config",
+    "core",
     "d",
     "e",
+    "from_json",
     "h",
+    "lazy_attribute",
+    "ldap",
+    "ldif",
     "m",
     "main",
-    "meltano_c",
+    "meltano",
     "p",
     "r",
     "s",
@@ -72,6 +80,8 @@ __all__: tuple[str, ...] = (
     "settings",
     "t",
     "tap_ldap",
+    "to_json",
+    "to_jsonable_python",
     "u",
     "x",
 )
@@ -79,7 +89,6 @@ __all__: tuple[str, ...] = (
 _LAZY_IMPORTS = MappingProxyType(
     build_lazy_import_map(
         MappingProxyType({
-            ".__version__": ("FlextTapLdapVersion",),
             "._config": ("FlextTapLdapConfig", "config"),
             "._settings": ("FlextTapLdapSettings", "settings"),
             ".api": ("FlextTapLdapService", "tap_ldap"),
@@ -92,9 +101,14 @@ _LAZY_IMPORTS = MappingProxyType(
             ".services.extract": ("FlextTapLdapExtractService",),
             ".typings": ("FlextTapLdapTypes", "t"),
             ".utilities": ("FlextTapLdapUtilities", "u"),
-            "flext_core": ("d", "e", "h", "r", "x"),
+            "flext_cli": ("cli",),
+            "flext_core": ("core", "d", "e", "h", "lazy_attribute", "r", "x"),
+            "flext_ldap": ("ldap",),
+            "flext_ldif": ("ldif",),
+            "flext_meltano": ("meltano",),
+            "pydantic_core": ("from_json", "to_json", "to_jsonable_python"),
         }),
-        alias_groups=MappingProxyType({"flext_meltano": (("meltano_c", "c"),)}),
+        alias_groups=MappingProxyType({}),
         sort_keys=False,
     )
 )
--- tests/__init__.py
+++ tests/__init__.py
@@ -9,9 +9,30 @@ from typing import TYPE_CHECKING
 from flext_core.lazy import build_lazy_import_map, install_lazy_exports
 
 if TYPE_CHECKING:
-    from flext_tests import FlextTestsConstants, d, e, h, r, td, tf, tk, tm, tv, x
+    from flext_cli import cli
+    from flext_infra import docs_main, infra
+    from flext_ldap import ldap
+    from flext_ldif import ldif
+    from flext_meltano import meltano
+    from flext_tests import (
+        active_rules,
+        api,
+        config,
+        discover_repository_root,
+        install_local_packages,
+        load_infra_report,
+        settings,
+        split_csv,
+        td,
+        tf,
+        tk,
+        tm,
+        tv,
+    )
+    from pydantic_core import from_json, to_json, to_jsonable_python
 
-    from flext_tap_ldap import FlextTapLdapConstants
+    from flext_core import core, d, e, h, lazy_attribute, r, x
+    from flext_tap_ldap import main, tap_ldap
 
     from . import e2e, unit
     from .base import TestsFlextTapLdapServiceBase, TestsFlextTapLdapServiceBase as s
@@ -22,8 +43,6 @@ if TYPE_CHECKING:
     from .typings import TestsFlextTapLdapTypes, TestsFlextTapLdapTypes as t
     from .utilities import TestsFlextTapLdapUtilities, TestsFlextTapLdapUtilities as u
 __all__: tuple[str, ...] = (
-    "FlextTapLdapConstants",
-    "FlextTestsConstants",
     "TestsFlextTapLdapConstants",
     "TestsFlextTapLdapModels",
     "TestsFlextTapLdapProtocols",
@@ -31,20 +50,41 @@ __all__: tuple[str, ...] = (
     "TestsFlextTapLdapSettings",
     "TestsFlextTapLdapTypes",
     "TestsFlextTapLdapUtilities",
+    "active_rules",
+    "api",
     "c",
+    "cli",
+    "config",
+    "core",
     "d",
+    "discover_repository_root",
+    "docs_main",
     "e",
     "e2e",
+    "from_json",
     "h",
+    "infra",
+    "install_local_packages",
+    "lazy_attribute",
+    "ldap",
+    "ldif",
+    "load_infra_report",
     "m",
+    "main",
+    "meltano",
     "p",
     "r",
     "s",
+    "settings",
+    "split_csv",
     "t",
+    "tap_ldap",
     "td",
     "tf",
     "tk",
     "tm",
+    "to_json",
+    "to_jsonable_python",
     "tv",
     "u",
     "unit",
@@ -63,20 +103,29 @@ _LAZY_IMPORTS = MappingProxyType(
             ".typings": ("TestsFlextTapLdapTypes", "t"),
             ".unit": ("unit",),
             ".utilities": ("TestsFlextTapLdapUtilities", "u"),
-            "flext_tap_ldap": ("FlextTapLdapConstants",),
+            "flext_cli": ("cli",),
+            "flext_core": ("core", "d", "e", "h", "lazy_attribute", "r", "x"),
+            "flext_infra": ("docs_main", "infra"),
+            "flext_ldap": ("ldap",),
+            "flext_ldif": ("ldif",),
+            "flext_meltano": ("meltano",),
+            "flext_tap_ldap": ("main", "tap_ldap"),
             "flext_tests": (
-                "FlextTestsConstants",
-                "d",
-                "e",
-                "h",
-                "r",
+                "active_rules",
+                "api",
+                "config",
+                "discover_repository_root",
+                "install_local_packages",
+                "load_infra_report",
+                "settings",
+                "split_csv",
                 "td",
                 "tf",
                 "tk",
                 "tm",
                 "tv",
-                "x",
             ),
+            "pydantic_core": ("from_json", "to_json", "to_jsonable_python"),
         }),
         alias_groups=MappingProxyType({}),
         sort_keys=False,
