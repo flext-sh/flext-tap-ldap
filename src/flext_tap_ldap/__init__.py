e04c3962e8dab9b5b52944b4225478138b66ff9e
Marlon Costa <marlon.costa@datacosmos.com.br>
2026-09-22 21:18:37 -0300

    chore(gen): adopt the generator's blank-line render over the merge

--- src/flext_tap_ldap/__init__.py
+++ src/flext_tap_ldap/__init__.py
@@ -20,12 +20,20 @@ from .__version__ import (
 )
 
 if TYPE_CHECKING:
-    from flext_cli import cli
-    from flext_ldap import ldap
-    from flext_ldif import ldif
+    from flext_ldap import (
+        adapters,
+        cli,
+        core,
+        d,
+        e,
+        h,
+        lazy_attribute,
+        ldap,
+        ldif,
+        r,
+        x,
+    )
     from flext_meltano import meltano
-
-    from flext_core import core, d, e, h, lazy_attribute, r, x
 
     from . import services
     from ._config import FlextTapLdapConfig, config
@@ -60,6 +68,7 @@ __all__: tuple[str, ...] = (
     "__url__",
     "__version__",
     "__version_info__",
+    "adapters",
     "c",
     "cli",
     "config",
@@ -99,10 +108,19 @@ _LAZY_IMPORTS = MappingProxyType(
             ".services.extract": ("FlextTapLdapExtractService",),
             ".typings": ("FlextTapLdapTypes", "t"),
             ".utilities": ("FlextTapLdapUtilities", "u"),
-            "flext_cli": ("cli",),
-            "flext_core": ("core", "d", "e", "h", "lazy_attribute", "r", "x"),
-            "flext_ldap": ("ldap",),
-            "flext_ldif": ("ldif",),
+            "flext_ldap": (
+                "adapters",
+                "cli",
+                "core",
+                "d",
+                "e",
+                "h",
+                "lazy_attribute",
+                "ldap",
+                "ldif",
+                "r",
+                "x",
+            ),
             "flext_meltano": ("meltano",),
         }),
         alias_groups=MappingProxyType({}),
